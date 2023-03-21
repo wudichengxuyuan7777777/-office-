00414674b2714f426ac9fa5281aec2763ec11bd5
wudiwudi <295243356@qq.com>
2023-03-21 18:14:51 +0800

    feat: log modify

--- 无敌爬虫/无敌爬虫.py
+++ 无敌爬虫/无敌爬虫.py
@@ -8,7 +8,12 @@ from time import sleep
 # 代码区
 # 存储数据的无敌列表
 wudi_list = []
+# 询问爬几个链接
+wudi = input('爬几个链接？')
+for x in range(int(wudi)):
+    # 询问爬什么链接
+    wudi = input('爬什么链接？')
+    wudi_list.append(wudi)
 # 询问爬几次
-wudi = input()
+wudi = input('爬几次链接？')
 wudi_list.append(int(wudi))
-
