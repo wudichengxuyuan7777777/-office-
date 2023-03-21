086a23bc03afa58b6b8efdf95fe075cc5430a3ac
wudiwudi <295243356@qq.com>
2023-03-21 18:16:10 +0800

    feat: log modify

--- 无敌爬虫/无敌爬虫.py
+++ 无敌爬虫/无敌爬虫.py
@@ -4,7 +4,6 @@ import os
 import office
 from time import sleep
 
-
 # 代码区
 # 存储数据的无敌列表
 wudi_list = []
@@ -17,3 +16,5 @@ for x in range(int(wudi)):
 # 询问爬几次
 wudi = input('爬几次链接？')
 wudi_list.append(int(wudi))
+# 打印列表
+print(wudi_list)
