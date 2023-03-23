1c8f6e951ad700817de0ac05b3b3698a597efd1d
wudiwudi <295243356@qq.com>
2023-03-23 20:10:16 +0800

    wudi

--- wudiwangzhan/manage.py
+++ wudiwangzhan/manage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
+
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wudiwangzhan.settings')
